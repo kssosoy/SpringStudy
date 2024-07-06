04cec1035e83cdf95f212aa64f83739414ba4488
Kim So Yeon <qws1566@naver.com>
2024-07-06 15:14:40 +0900

    :sparkles: feat: 내가 작성한 리뷰 목록 조회

--- src/main/java/jpabook/springbootjpa/repository/ReviewRepository.java
+++ src/main/java/jpabook/springbootjpa/repository/ReviewRepository.java
@@ -1,5 +1,6 @@
 package jpabook.springbootjpa.repository;
 
+import jpabook.springbootjpa.domain.Member;
 import jpabook.springbootjpa.domain.Review;
 import jpabook.springbootjpa.domain.Store;
 import org.springframework.data.domain.Page;
@@ -8,4 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     Page<Review> findAllByStore(Store store, PageRequest pageRequest);
+
+    Page<Review> findAllByMember (Member member, PageRequest pageRequest);
 }
--- src/main/java/jpabook/springbootjpa/web/dto/MemberResponseDTO.java
+++ src/main/java/jpabook/springbootjpa/web/dto/MemberResponseDTO.java
@@ -5,7 +5,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class MemberResponseDTO {
     @Builder
@@ -24,5 +26,29 @@ public class MemberResponseDTO {
     public static class challengeMissionDTO{
         Long memberMissionId;
         LocalDateTime createdAt;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreViewListDTO{
+        List<ReviewPreViewDTO> reviewList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreViewDTO{
+        String ownerNickname;
+        Float score;
+        String body;
+        LocalDate createdAt;
     }
 }
